9cc000c77cd66c6f03e634e5a98461f310d0617f
Damian1457 <damianwasik.coach@gmail.com>
2023-12-18 21:17:10 +0100

    I added a service to handle sending the request and converting the response, as well as unit tests to the methods.

---
+++ src/test/java/pl/wasik/damian/java/speakjokes/service/ChuckNorrisJokesServiceTest.java
@@ -0,0 +1,54 @@
+package pl.wasik.damian.java.speakjokes.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import pl.wasik.damian.java.speakjokes.api.ChuckNorrisJokesApiResponse;
+
+import java.io.IOException;
+
+class ChuckNorrisJokesServiceTest {
+
+    @Test
+    void getResponse() throws IOException {
+        //Given
+        ChuckNorrisJokesService chuckNorrisJokesService = new ChuckNorrisJokesService();
+        String url = "https://api.chucknorris.io/jokes/random";
+
+        //When
+        String response = chuckNorrisJokesService.getResponse(url);
+
+        //Then
+        Assertions.assertNotNull(response, "response is NULL");
+    }
+
+    @Test
+    void convert() {
+        //Given
+        ChuckNorrisJokesService chuckNorrisJokesService = new ChuckNorrisJokesService();
+
+        //When
+        ChuckNorrisJokesApiResponse chuckNorrisJokesApiResponse = chuckNorrisJokesService.convert("{\n" +
+                "\"icon_url\" : \"https://assets.chucknorris.host/img/avatar/chuck-norris.png\",\n" +
+                "\"id\" : \"nl7wJ4kaSHqjLxo1tm5GmA\",\n" +
+                "\"url\" : \"\",\n" +
+                "\"value\" : \"When Chuck Norris breaks wind, it stays broken.\"\n" +
+                "}"
+        );
+
+        //Then
+        Assertions.assertNotNull(chuckNorrisJokesApiResponse, "response is NULL");
+
+    }
+
+    @Test
+    void randomJoke() {
+        //Given
+        ChuckNorrisJokesService chuckNorrisJokesService = new ChuckNorrisJokesService();
+
+        //When
+        ChuckNorrisJokesApiResponse chuckNorrisJokesApiResponse = chuckNorrisJokesService.randomJoke();
+
+        //Then
+        Assertions.assertNotNull(chuckNorrisJokesApiResponse, "response is NULL");
+    }
+}
